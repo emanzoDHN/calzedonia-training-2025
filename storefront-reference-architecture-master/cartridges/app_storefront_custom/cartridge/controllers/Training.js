13bef96f464630776cc4fab3084846f4f19f199c
emanzoDHN <emiliana.manzo@calzedonia.it>
2025-03-10 14:21:04 +0100

    introduction to form

--- storefront-reference-architecture-master/cartridges/app_storefront_custom/cartridge/controllers/Training.js
+++ storefront-reference-architecture-master/cartridges/app_storefront_custom/cartridge/controllers/Training.js
@@ -1,20 +1,34 @@
 'use strict';
 
 var server = require('server');
-var ProductMgr = require('dw/catalog/ProductMgr');
 
-server.get('Start', function (req, res, next) {
-    var product = ProductMgr.getProduct("008884303989M");
-    var color = product.custom.color;
-    
-    res.render('helloworld', {
-        color : color,
-        product : product
+server.get("Show", function(req, res, next) {
+    var URLUtils = require("dw/web/URLUtils");
+    var Resource = require("dw/web/Resource");
+
+    var profileForm = server.forms.getForm("training");
+    profileForm.clear();
+
+    res.render("trainingform", {
+        title: Resource.msg("training.form.title.submit", "forms", null),
+        profileForm: profileForm,
+        actionUrl: URLUtils.url("Training-SubmitRegistration").toString()
     });
 
     next();
-
 });
 
+server.post("SubmitRegistration", function(req, res, next) {
+        var Resource = require("dw/web/Resource");
+        var URLUtils = require("dw/web/URLUtils");
+        var profileForm = server.forms.getForm("training");
+        res.render("trainingform", {
+            title: Resource.msg("training.form.title.edit", "forms", null),
+            profileForm: profileForm,
+            actionUrl: URLUtils.url("Training-SubmitRegistration").toString()
+        });
+
+        next();
+    });
 
 module.exports = server.exports();
