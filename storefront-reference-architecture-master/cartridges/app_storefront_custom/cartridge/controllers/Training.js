cbeb1973058eb0caa8f30559593fd7e4c19328c4
emanzoDHN <emiliana.manzo@calzedonia.it>
2025-03-10 15:00:52 +0100

    introduction to custom object

---
+++ storefront-reference-architecture-master/cartridges/app_storefront_custom/cartridge/controllers/Training.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var server = require('server');
+var ProductMgr = require('dw/catalog/ProductMgr');
+
+server.get('Start', function (req, res, next) {
+    var product = ProductMgr.getProduct("008884303989M");
+    var color = product.custom.color;
+    
+    res.render('helloworld', {
+        color : color,
+        product : product,
+        
+    });
+
+    next();
+
+});
+
+
+module.exports = server.exports();
